de9f30113738d3f0fe5c7602c9f145016b23dfb0
Yota Toyama <raviqqe@gmail.com>
2016-12-14 17:18:44 +0900

    Pass hosts information to docker run

--- lib/lib.rb
+++ lib/lib.rb
@@ -1,7 +1,23 @@
-require_relative "docker-rake-tasks/docker"
+require 'resolv'
+
+require_relative 'docker-rake-tasks/docker'
+require_relative '../config'
 
 
 def docker_tasks name, run_options='', &block
-  DockerTasks.new("raviqqe", "Yota Toyama <raviqqe@gmail.com>", "ubuntu")
+  DockerTasks.new('raviqqe', 'Yota Toyama <raviqqe@gmail.com>', 'ubuntu')
       .create name, run_options, &block
 end
+
+def hostname_to_ip hostname
+  Resolv::Hosts.new().getaddress hostname
+end
+
+def hostname_to_pair hostname
+  [hostname, hostname_to_ip(hostname)]
+end
+
+DOCKER_HOSTS_OPTION = [*MASTERS, *AGENTS].map(&method(:hostname_to_pair))
+                                         .map do |h, ip|
+  "--add-host #{h}:#{ip}"
+end.join ' '
